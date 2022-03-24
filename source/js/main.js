614e0442554e71b41419c701cc49ddab95adbd80
yatsev1ch <ayatsevdeveloper@gmail.com>
2022-03-25 01:03:20 +0300

    Убрана build из gitignore

--- source/js/main.js
+++ source/js/main.js
@@ -1,18 +1,43 @@
+const page = document.querySelector('.page');
 const header = document.querySelector('.page-header');
 const menu = document.querySelector('.page-header__wrapper');
-const menuButton = document.querySelector('.page-header__menu-button');
+const menuOverlay = document.querySelector('.page-header__menu-overlay');
 
 header.classList.remove('page-header--no-js');
 menu.classList.remove('page-header__wrapper--no-js');
 
-const menuClickHandler = () => {
-  if (menu.classList.contains('page-header__wrapper--opened')) {
-    menu.classList.remove('page-header__wrapper--opened');
-    return;
-  }
+const isMenuOpened = () => menu.classList.contains('page-header__wrapper--opened');
+const isMenuButton = (target) => target.classList.contains('page-header__menu-button');
+const isMenuLink = (target) => target.classList.contains('page-header__nav-link');
+
+const openMenu = () => {
   menu.classList.add('page-header__wrapper--opened');
+  page.classList.add('page--with-menu');
 };
 
-menuButton.addEventListener('click', () => {
-  menuClickHandler();
+const closeMenu = () => {
+  menu.classList.remove('page-header__wrapper--opened');
+  page.classList.remove('page--with-menu');
+};
+
+const menuClickHandler = () => {
+  if (isMenuOpened()) {
+    closeMenu();
+    return;
+  }
+  openMenu();
+};
+
+menu.addEventListener('click', (evt) => {
+  switch (true) {
+    case isMenuButton(evt.target):
+      menuClickHandler();
+      break;
+    case isMenuLink(evt.target):
+      closeMenu();
+  }
+});
+
+menuOverlay.addEventListener('click', () => {
+  closeMenu();
 });
